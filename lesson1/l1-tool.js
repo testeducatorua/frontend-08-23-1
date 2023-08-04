75f9837ad5d8d8bda1a05c62e9fa9eae73e9111d
testeducatorua <testeducatorua@gmail.com>
2023-08-04 20:18:32 +0300

    base example start code

--- lesson1/l1-tool.js
+++ lesson1/l1-tool.js
@@ -1,1 +1,14 @@
-var num1 = 5;
+// let num1: number = 5;
+// let text: string = 'message';
+// const baseInfo = 99.12;
+// console.log(num1, text, baseInfo);
+// let text = 'message';
+// text()
+// const userInfo = {
+//     name: 'Ivan',
+//     age: 40,
+//     geo: ['UK', 'Lviv'] 
+// }
+// userInfo.family
+var text = 'message';
+// text.toLocaleUppercase
--- lesson1/l1-tool.ts
+++ lesson1/l1-tool.ts
@@ -1,1 +1,19 @@
-let num1 = 5
+// let num1: number = 5;
+// let text: string = 'message';
+// const baseInfo = 99.12;
+
+// console.log(num1, text, baseInfo);
+
+// let text = 'message';
+// text()
+
+// const userInfo = {
+//     name: 'Ivan',
+//     age: 40,
+//     geo: ['UK', 'Lviv'] 
+// }
+// userInfo.family
+
+let text = 'message';
+
+// text.toLocaleUppercase
