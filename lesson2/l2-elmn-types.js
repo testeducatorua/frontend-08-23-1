6b1452cba6550abb576965f5ae6c1af99678ab7d
testeducatorua <testeducatorua@gmail.com>
2023-08-09 16:07:26 +0300

    2 lesson - elementary types

---
+++ lesson2/l2-elmn-types.js
@@ -0,0 +1,34 @@
+// let test;
+// let nameUser 
+// function greet(name: string) {
+//     console.log('Hello, ' + name);
+// }
+// greet(nameUser)
+// function greet(person: string, date: Date) {
+//     console.log(`Hello ${person}, today is ${date.toDateString()}!`);
+// }
+// greet('Orest',new Date())
+// let obj: any = {x: 0}
+// obj.testMethod()
+// obj()
+// obj.bar = 100
+// obj = 'hello'
+// const n: number = obj
+// let userName: string = 'Ivan'
+// let age: number = 34
+// let indicator: boolean = true
+// let fruits: string[] = ['apple', 'apple', 'cherry']
+// let indexPost: number[] = [343434, 343545654, 12121]
+// let something: any = 'kfas;dlkas;das'
+// let somethingD: any = 45234234
+// function greet(name:string) {
+//     console.log('Hello, ' + name.toUpperCase());
+// }
+// greet('orest')
+// function randomFavor(): number {
+//     return 25
+// }
+const fruits = ['apple', 'apple', 'cherry'];
+fruits.forEach(function (fruitItem) {
+    console.log(fruitItem.toUpperCase());
+});
---
+++ lesson2/l2-elmn-types.ts
@@ -0,0 +1,58 @@
+// let test;
+
+// let nameUser 
+// function greet(name: string) {
+//     console.log('Hello, ' + name);
+
+// }
+// greet(nameUser)
+
+// function greet(person: string, date: Date) {
+//     console.log(`Hello ${person}, today is ${date.toDateString()}!`);
+// }
+// greet('Orest',new Date())
+
+// let obj: any = {x: 0}
+// obj.testMethod()
+// obj()
+// obj.bar = 100
+// obj = 'hello'
+// const n: number = obj
+
+// let userName: string = 'Ivan'
+// let age: number = 34
+// let indicator: boolean = true
+// let fruits: string[] = ['apple', 'apple', 'cherry']
+// let indexPost: number[] = [343434, 343545654, 12121]
+// let something: any = 'kfas;dlkas;das'
+// let somethingD: any = 45234234
+
+// function greet(name:string) {
+//     console.log('Hello, ' + name.toUpperCase());
+// }
+// greet('orest')
+
+// function randomFavor(): number {
+//     return 25
+// }
+
+// const fruits: string[] = ['apple', 'apple', 'cherry']
+
+// fruits.forEach(function (fruitItem) {
+//     console.log(fruitItem.toUpperCase());
+
+// })
+
+// function printCoords(point: { x: number; y: number; }) {
+//     console.log('x ' + point.x);
+//     console.log('y ' + point.y);
+
+// }
+
+// printCoords({ x: 3434, y: 4545 })
+
+function printName(namesObj: {first: string; last?: string}) {
+    // ...
+}
+printName({ first: 'Stepan'})
+printName({ first: 'Stepan', last: 'Stepanenko'})
